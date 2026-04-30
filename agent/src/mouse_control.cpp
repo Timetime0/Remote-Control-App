a5be136dcaae2746e807c7db71f93a423413266f
Timetime24 <2588025960@student.hcmus.edu.vn>
2026-04-30 21:55:15 +0700

    feat: update mouse control in mac

--- agent/src/mouse_control.cpp
+++ agent/src/mouse_control.cpp
@@ -2,12 +2,29 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#elif defined(__APPLE__)
+#include <ApplicationServices/ApplicationServices.h>
 #endif
 
 std::string mouseMoveJson(int x, int y) {
 #ifdef _WIN32
     SetCursorPos(x, y);
 
+    return "{"
+        "\"ok\":true,"
+        "\"command\":\"MOUSE_MOVE\","
+        "\"message\":\"mouse_moved\""
+        "}";
+#elif defined(__APPLE__)
+    const CGPoint point = CGPointMake(static_cast<CGFloat>(x), static_cast<CGFloat>(y));
+    CGEventRef moveEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventMouseMoved, point, kCGMouseButtonLeft
+    );
+    if (!moveEvent) {
+        return "{\"ok\":false,\"command\":\"MOUSE_MOVE\",\"message\":\"event_create_failed\"}";
+    }
+    CGEventPost(kCGHIDEventTap, moveEvent);
+    CFRelease(moveEvent);
     return "{"
         "\"ok\":true,"
         "\"command\":\"MOUSE_MOVE\","
@@ -30,6 +47,39 @@ std::string mouseLeftClickJson(int x, int y) {
         "\"command\":\"MOUSE_LEFT_CLICK\","
         "\"message\":\"left_click_done\""
         "}";
+#elif defined(__APPLE__)
+    const CGPoint point = CGPointMake(static_cast<CGFloat>(x), static_cast<CGFloat>(y));
+
+    CGEventRef moveEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventMouseMoved, point, kCGMouseButtonLeft
+    );
+    CGEventRef downEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventLeftMouseDown, point, kCGMouseButtonLeft
+    );
+    CGEventRef upEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventLeftMouseUp, point, kCGMouseButtonLeft
+    );
+
+    if (!moveEvent || !downEvent || !upEvent) {
+        if (moveEvent) CFRelease(moveEvent);
+        if (downEvent) CFRelease(downEvent);
+        if (upEvent) CFRelease(upEvent);
+        return "{\"ok\":false,\"command\":\"MOUSE_LEFT_CLICK\",\"message\":\"event_create_failed\"}";
+    }
+
+    CGEventPost(kCGHIDEventTap, moveEvent);
+    CGEventPost(kCGHIDEventTap, downEvent);
+    CGEventPost(kCGHIDEventTap, upEvent);
+
+    CFRelease(moveEvent);
+    CFRelease(downEvent);
+    CFRelease(upEvent);
+
+    return "{"
+        "\"ok\":true,"
+        "\"command\":\"MOUSE_LEFT_CLICK\","
+        "\"message\":\"left_click_done\""
+        "}";
 #else
     return "{\"ok\":false,\"command\":\"MOUSE_LEFT_CLICK\",\"message\":\"not_supported\"}";
 #endif
@@ -41,6 +91,39 @@ std::string mouseRightClickJson(int x, int y) {
 
     mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
     mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+
+    return "{"
+        "\"ok\":true,"
+        "\"command\":\"MOUSE_RIGHT_CLICK\","
+        "\"message\":\"right_click_done\""
+        "}";
+#elif defined(__APPLE__)
+    const CGPoint point = CGPointMake(static_cast<CGFloat>(x), static_cast<CGFloat>(y));
+
+    CGEventRef moveEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventMouseMoved, point, kCGMouseButtonRight
+    );
+    CGEventRef downEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventRightMouseDown, point, kCGMouseButtonRight
+    );
+    CGEventRef upEvent = CGEventCreateMouseEvent(
+        nullptr, kCGEventRightMouseUp, point, kCGMouseButtonRight
+    );
+
+    if (!moveEvent || !downEvent || !upEvent) {
+        if (moveEvent) CFRelease(moveEvent);
+        if (downEvent) CFRelease(downEvent);
+        if (upEvent) CFRelease(upEvent);
+        return "{\"ok\":false,\"command\":\"MOUSE_RIGHT_CLICK\",\"message\":\"event_create_failed\"}";
+    }
+
+    CGEventPost(kCGHIDEventTap, moveEvent);
+    CGEventPost(kCGHIDEventTap, downEvent);
+    CGEventPost(kCGHIDEventTap, upEvent);
+
+    CFRelease(moveEvent);
+    CFRelease(downEvent);
+    CFRelease(upEvent);
 
     return "{"
         "\"ok\":true,"
